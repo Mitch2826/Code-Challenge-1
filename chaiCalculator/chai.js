13309be85435900c08c27e67069ce0045c13fff2
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-06-22 03:00:17 +0300

    Challenge 1

---
+++ chaiCalculator/chai.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')();
+
+// Prompt the user:
+const input = prompt("Karibu! How many cups of Chai Bora would you like to make?")
+const numberOfCups = Number(input); //Ensures the string input is converted to number
+
+//function to calculate chai ingredients
+function calculateChaiIngredients(numberOfCups){
+    const water = 200 * numberOfCups; //ml
+    const milk = 50 * numberOfCups;//ml
+    const teaLeaves = 1 * numberOfCups; //tablespoons
+    const sugar = 2 * numberOfCups; //teaspoons
+
+    console.log(`To make ${numberOfCups} cups of Kenyan Chai, you will need:`);
+    console.log(`Water: ${water} ml`);
+    console.log(`Milk: ${milk} ml`);
+    console.log(`Tea Leaves (Majani): ${teaLeaves} tablespoons`);
+    console.log(`Sugar (Sukari): ${sugar} teaspoons\n`);
+    console.log(`Enjoy your Chai Bora!`);
+}
+calculateChaiIngredients(numberOfCups);
+
+//validation of the user input
+if (numberOfCups <= 0 || isNaN(numberOfCups)){
+    console.log("Please enter valid number of cups!");
+} else {
+    calculateChaiIngredients(numberOfCups);
+}
