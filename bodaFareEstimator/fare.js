258009b9e13fdb7c0321972e7b0df6d7e87fc36d
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-06-22 04:29:23 +0300

    Challenge 2

---
+++ bodaFareEstimator/fare.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt-sync')(); //imports prompt-sync
+
+//prompts user for distance
+const input = prompt("Unafika wapi Mkubwa? Kilometer ngapi?: ");
+const distanceInKm = Number(input); //converts string input to a number
+
+//function to calculate fare
+function calculateBodaFare(distance){
+const baseFare = 50;
+const chargePerKm= 15;
+
+let charge= distance * chargePerKm;// calculates the total charge per kilometre
+let totalFare = baseFare + (distance * chargePerKm);// calculates the total fare
+    //logs console output
+    console.log(`Uko kwote? Io ni ${distance} km: `);
+    console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
+    console.log(`Mpaka Uko: KES ${charge}`); 
+    console.log(`Total: KES ${totalFare}\n`);
+    console.log("Panda Pikipiki!")
+}
+//validates user inputs using if else
+if(isNaN(distanceInKm) || distanceInKm <= 0){
+    console.log("Tafadhali andika nambari sahihi!")//logs if at least one of the conditions is true
+}else{
+calculateBodaFare(distanceInKm); // calls the calculateBodaFare function with distanceInKm as the argument
+}
